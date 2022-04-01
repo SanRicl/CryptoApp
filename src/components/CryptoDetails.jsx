7a674657c3fb25c8f5ad7ba72339325248be9580
sanricl <sandersonricl@gmail.com>
2022-04-01 09:59:22 -0300

    Commit Initial

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const CryptoDetails = () => {
+ 
+  
+    return (
+    <div style={{textAlign: "center", fontSize: "2em"}}>Nothing here... yet :P</div>
+  )
+}
+
+export default CryptoDetails
